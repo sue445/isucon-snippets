53fa72cd3e7aba6e5ad47d782b6dd0625ffb8ccc
sue445 <sue445@sue445.net>
2021-07-29 21:20:16 +0900

    Add newrelic.rb

---
+++ config/newrelic.rb
@@ -0,0 +1,38 @@
+require "newrelic_rpm"
+
+# see https://github.com/shirokanezoo/isucon9f/commit/db8ef5934666fde3e23c17a04c4394b12a343110#diff-e90610944058d63767be863ddbd31bfd
+class NRMysql2Client < Mysql2::Client
+  LOG_FILE = "/tmp/sql.log"
+
+  def initialize(*args)
+    @logger = Logger.new(LOG_FILE)
+    super
+  end
+
+  def self.parse_table(sql)
+    # FIXME
+    sql[/(?<=[ `])(users|events|sheets|reservations|administrators)(?=[ `])/] || 'other'
+  end
+
+  def self.with_newrelic(sql)
+    callback = -> (result, metrics, elapsed) do
+      NewRelic::Agent::Datastores.notice_sql(sql, metrics, elapsed)
+    end
+    op = sql[/^(select|insert|update|delete|begin|commit|rollback)/i] || 'other'
+
+    table = parse_table(sql)
+
+    NewRelic::Agent::Datastores.wrap('MySQL', op, table, callback) do
+      yield
+    end
+  end
+
+  def query(sql, *args)
+    table = NRMysql2Client.parse_table(sql)
+    @logger.info "[#{table}] #{sql}"
+
+    NRMysql2Client.with_newrelic(sql) do
+      super
+    end
+  end
+end
