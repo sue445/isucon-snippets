1e0c821b3d2f2c019c7f0aec5dbe32fff4f6878d
GO Sueyoshi <sue445@users.noreply.github.com>
2021-08-01 00:49:04 +0900

    Update sentry.rb

--- webapp/ruby/config/sentry.rb
+++ webapp/ruby/config/sentry.rb
@@ -3,6 +3,9 @@ require "systemu"
 
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
+
+  # TODO: sentryを無効化する時はenabled_environmentsを空にする
+  config.enabled_environments = %w[production]
 end
 
 module SentryMethods
