3b437f64d17906285abe1174b26e74fec0f0fc69
sue445 <sue445@sue445.net>
2021-08-09 09:51:49 +0900

    Add config/redis

---
+++ webapp/ruby/config/redis.rb
@@ -0,0 +1,21 @@
+require "redis"
+require "connection_pool"
+
+$redis = ConnectionPool::Wrapper.new(size: 32, timeout: 3) { Redis.new(host: ENV["REDIS_HOST"]) }
+
+module RedisMethods
+  def with_redis(cache_key, enabled: true)
+    unless enabled
+      return yield
+    end
+
+    cached_response = $redis.get(cache_key)
+    return cached_response if cached_response
+
+    actual = yield
+
+    $redis.set(cache_key, actual)
+
+    actual
+  end
+end
