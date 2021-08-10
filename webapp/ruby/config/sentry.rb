c89e8e85bb51a5c50b3d273b4a68ac0cd0d87a1d
sue445 <sue445@sue445.net>
2021-08-10 22:37:13 +0900

    Omit config.dsn

--- webapp/ruby/config/sentry.rb
+++ webapp/ruby/config/sentry.rb
@@ -1,8 +1,6 @@
 require "sentry-ruby"
 
 Sentry.init do |config|
-  config.dsn = ENV["SENTRY_DSN"]
-
   # TODO: sentryを無効化する時はenabled_environmentsを空にする
   config.enabled_environments = %w[production development]
 end
