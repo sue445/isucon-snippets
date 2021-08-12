823a96b96cf6cb48df182f97e74c03e4d7b53f00
sue445 <sue445@sue445.net>
2021-08-12 23:51:04 +0900

    Remove stackprof_methods.rb

--- webapp/ruby/dummy_app.rb
+++ webapp/ruby/dummy_app.rb
@@ -9,22 +9,13 @@ ENV["RACK_ENV"] = "development"
 require_relative "./config/sentry"
 
 require_relative "./config/sentry_methods"
-require_relative "./config/stackprof_methods"
 
 class App < Sinatra::Base
-  include StackprofMethods
-
   get "/" do
     "It works"
   end
 
   get "/sentry_test" do
     raise "sentry test"
-  end
-
-  get "/users/:id" do
-    with_stackprof(true) do
-      "user #{params[:id]}"
-    end
   end
 end
