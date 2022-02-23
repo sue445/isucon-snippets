c99e81f871b12d1e6b8d9df271c3e3628b6af809
sue445 <sue445@sue445.net>
2022-02-23 23:56:17 +0900

    Add oj_to_json_patch.rb

---
+++ webapp/ruby/config/oj_to_json_patch.rb
@@ -0,0 +1,16 @@
+# to_jsonをojに置き換えるモンキーパッチ
+require "oj"
+
+::Oj.default_options = { mode: :compat }
+
+class Hash
+  def to_json
+    ::Oj.dump(self)
+  end
+end
+
+class Array
+  def to_json
+    ::Oj.dump(self)
+  end
+end
