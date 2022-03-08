8ce41a7b9b0ca125cfde5395541583c73fca35d9
sue445 <sue445@sue445.net>
2022-03-09 01:16:25 +0900

    Add sidekiq_methods.rb

---
+++ webapp/ruby/config/sidekiq_methods.rb
@@ -0,0 +1,12 @@
+require "sidekiq/api"
+
+module SidekiqMethods
+  # c.f.
+  # * https://gist.github.com/wbotelhos/fb865fba2b4f3518c8e533c7487d5354
+  # * https://qiita.com/ts-3156/items/ec4608c7c9cf1494bcc1
+  def clear_sidekiq_all_queue
+    Sidekiq::Queue.all.each do |queue|
+      queue.clear
+    end
+  end
+end
