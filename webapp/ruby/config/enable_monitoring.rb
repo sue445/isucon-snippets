7a9bc5e7f71a7e55debb9fbbfd1e3c67fc1b4210
Go Sueyoshi <sue445@users.noreply.github.com>
2022-03-19 03:35:58 +0900

    Update enable_monitoring.rb

--- webapp/ruby/config/enable_monitoring.rb
+++ webapp/ruby/config/enable_monitoring.rb
@@ -66,7 +66,9 @@ end
 
 ::Sinatra::Base.prepend(DatadogSinatraRouteingPathNamePatch)
 
-# Sidekiq::Workerをincludeした全workerクラスにモンキーパッチをあてる
+# Sidekiq::Workerをincludeした全workerクラスにモンキーパッチをあてる]
+# NOTE: Sidekiq::Workerがincludeされているかどうかを厳密に調べようとするとインスタンスを生成する必要があって遅くなるため、
+#       Sidekiq::Workerをincludeした時に生成されるクラスメソッド(sidekiq_options)の存在をチェックしている
 sidekiq_worker_classes = ObjectSpace.each_object(Class).select do |klass|
   klass.name&.end_with?("Worker") && klass.respond_to?(:sidekiq_options)
 end
