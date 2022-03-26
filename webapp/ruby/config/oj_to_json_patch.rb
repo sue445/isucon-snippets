be59b7c1ba46a11f3f18cd60747d27eb81232a8c
sue445 <sue445@sue445.net>
2022-03-27 02:32:48 +0900

    Tweak arg

--- webapp/ruby/config/oj_to_json_patch.rb
+++ webapp/ruby/config/oj_to_json_patch.rb
@@ -4,13 +4,13 @@ require "oj"
 ::Oj.default_options = { mode: :compat }
 
 class Hash
-  def to_json
+  def to_json(*)
     ::Oj.dump(self)
   end
 end
 
 class Array
-  def to_json
+  def to_json(*)
     ::Oj.dump(self)
   end
 end
