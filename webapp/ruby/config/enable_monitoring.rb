370f21116c7474883e8467d99a7824b5af696cf8
sue445 <sue445@sue445.net>
2022-03-27 02:48:28 +0900

    Remove stackprof

--- webapp/ruby/config/enable_monitoring.rb
+++ webapp/ruby/config/enable_monitoring.rb
@@ -1,6 +1,5 @@
-# ddtrace, Sentry, stackprofの一括有効＆一括無効を1ファイルのみで行うためのファイル
+# ddtrace, Sentryの一括有効＆一括無効を1ファイルのみで行うためのファイル
 require "sentry-ruby"
-require "stackprof"
 require "sinatra"
 require "ddtrace"
 require "mysql2"
@@ -9,40 +8,9 @@ Sentry.init do |config|
   config.enabled_environments = %w[production development]
 end
 
-def enabled_stackprof_path?(env)
-  return true
-
-  # TODO: 個別指定の場合はsave_everyを小さくしないとdumpが保存されないので一緒に変更すること
-  case env["REQUEST_METHOD"]
-  when "GET"
-    # case env["PATH_INFO"]
-    # when %r{^/users/[0-9]+$}
-    #   return true
-    # end
-
-  when "POST"
-    # case env["PATH_INFO"]
-    # when "/message"
-    #   return true
-    # end
-
-  end
-
-  false
-end
-
 # NOTE: 書くのをよく忘れるのでファイルをrequireした時点で自動でSentry::Rack::CaptureExceptionsnaなどが適用されるようにする
 class Sinatra::Base
   use Sentry::Rack::CaptureExceptions
-
-  use StackProf::Middleware,
-      mode: :cpu,
-      interval: 1000,
-      raw: true,
-      save_every: 100,
-      path: "tmp/stackprof/",
-      # 特定のPATHのみstackprofを有効化する
-      enabled: -> (env) { enabled_stackprof_path?(env) }
 end
 
 if DDTrace::VERSION::MAJOR >= 1
--- webapp/ruby/run-stackprof-webnav.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -xe
-
-stackprof-webnav -d tmp/stackprof/ -p 9293 -o 0.0.0.0
