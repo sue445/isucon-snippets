2c585f71ad82dd84cbde3dac9a7831c26e82a2c7
sue445 <sue445@sue445.net>
2022-06-11 14:49:58 +0900

    Refactor

--- setup.sh
+++ setup.sh
@@ -15,18 +15,21 @@ cp Rakefile ${TARGET_DIR}
 cp -R infra ${TARGET_DIR}
 set +x
 
-if [ -e "${TARGET_DIR}/webapp/ruby" ]; then
+function copy_to_ruby_dir() {
+  local ruby_dir=$1
+
   set -x
-  cp webapp/ruby/.rubocop.yml ${TARGET_DIR}/webapp/ruby
-  cp -R webapp/ruby/config ${TARGET_DIR}/webapp/ruby
+  cp webapp/ruby/.rubocop.yml ${ruby_dir}
+  cp -R webapp/ruby/config ${ruby_dir}
   set +x
+}
+
+if [ -e "${TARGET_DIR}/webapp/ruby" ]; then
+  copy_to_ruby_dir "${TARGET_DIR}/webapp/ruby"
 fi
 
 if [ -e "${TARGET_DIR}/ruby" ]; then
-  set -x
-  cp webapp/ruby/.rubocop.yml ${TARGET_DIR}/ruby
-  cp -R webapp/ruby/config ${TARGET_DIR}/ruby
-  set +x
+  copy_to_ruby_dir "${TARGET_DIR}/ruby"
 fi
 
 echo "Copy to ${TARGET_DIR}"
