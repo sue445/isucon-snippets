2298df571f9c9b9ec6ea84a3ad0f81bd0c3c3efd
sue445 <sue445@sue445.net>
2022-06-11 14:53:31 +0900

    Add gemfile_patch.rb

---
+++ webapp/ruby/gemfile_patch.rb
@@ -0,0 +1,20 @@
+# profiling and monitoring
+gem "ddtrace", ">= 1.0.0.beta1"
+gem "dogstatsd-ruby"
+gem "google-protobuf", "~> 3.0"
+gem "sentry-ruby"
+
+gem "oj"
+gem "parallel"
+gem "puma_worker_killer", require: false
+
+group :development do
+  gem "rubocop-isucon", github: "sue445/rubocop-isucon"
+end
+
+# 必要に応じて使う
+# gem "connection_pool"
+# gem "dalli"
+# gem "redis"
+# gem "sidekiq"
+# gem "sidekiq-cron"
