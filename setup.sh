02b26fcac371c9644a5c0334b6b105c2ad903261
sue445 <sue445@sue445.net>
2022-06-11 14:46:32 +0900

    Print command when only copying

--- setup.sh
+++ setup.sh
@@ -10,17 +10,23 @@ if [ -z "${TARGET_DIR}" ]; then
   exit 1
 fi
 
+set -x
 cp Rakefile ${TARGET_DIR}
 cp -R infra ${TARGET_DIR}
+set +x
 
 if [ -e "${TARGET_DIR}/webapp/ruby" ]; then
+  set -x
   cp webapp/ruby/.rubocop.yml ${TARGET_DIR}/webapp/ruby
   cp -R webapp/ruby/config ${TARGET_DIR}/webapp/ruby
+  set +x
 fi
 
 if [ -e "${TARGET_DIR}/ruby" ]; then
+  set -x
   cp webapp/ruby/.rubocop.yml ${TARGET_DIR}/ruby
   cp -R webapp/ruby/config ${TARGET_DIR}/ruby
+  set +x
 fi
 
 echo "Copy to ${TARGET_DIR}"
