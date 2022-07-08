93a25ba40c80f520e644d25ef74e248c18786fd9
sue445 <sue445@sue445.net>
2022-07-09 02:21:00 +0900

    Add datadog_thread_tracer

--- webapp/ruby/config/thread_helper.rb
+++ webapp/ruby/config/thread_helper.rb
@@ -1,48 +1,32 @@
 # サブスレッドのトレースをDatadogに送るためのヘルパー
+require "datadog_thread_tracer"
+
 class ThreadHelper
   def initialize
     @threads = []
-    @thread_count = 0
   end
 
-  # ブロック内をスレッドで実行する。ddtraceをrequireしてる場合はトレースも行う
-  # @param trace_name [String]
-  # @yield
-  def start_thread(trace_name = nil)
-    if defined? ::Datadog
-      @thread_count += 1
-      trace_name ||= "thread_#{@thread_count}"
-
-      # c.f. https://github.com/DataDog/dd-trace-rb/issues/1460
-      tracer = Datadog::Tracing.send(:tracer)
-      context = tracer.provider.context
-      @threads << Thread.new(trace_name, context) do |trace_name, context|
-        tracer = Datadog::Tracing.send(:tracer)
-        tracer.provider.context = context
-        Datadog::Tracing.trace(trace_name) do
-          yield
-        end
-      end
-    else
-      # ddtraceをrequireしていない場合はトレースを行わずスレッドを実行する
-      @threads << Thread.new do
-        yield
-      end
+  def trace(_trace_name = nil)
+    @threads << Thread.new do
+      yield
     end
   end
 
-  # スレッドが終了するまで待つ
   def join_threads
     @threads.each(&:join)
   end
 
-  # @yield
-  # @yieldparam [ThreadHelper]
   def self.within_threads
-    helper = ThreadHelper.new
+    if defined? ::Datadog
+      DatadogThreadTracer.trace do |thread_tracer|
+        yield thread_tracer
+      end
+    else
+      helper = ThreadHelper.new
 
-    yield helper
+      yield helper
 
-    helper.join_threads
+      helper.join_threads
+    end
   end
 end
