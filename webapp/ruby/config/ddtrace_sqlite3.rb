92e5e86b6ce628f6e576dbf5cd857fc1218aa5bb
sue445 <sue445@sue445.net>
2022-08-07 17:21:19 +0900

    Add ddtrace_sqlite3

---
+++ webapp/ruby/config/ddtrace_sqlite3.rb
@@ -0,0 +1,23 @@
+require "sqlite3"
+require "ddtrace"
+
+module DatadogSqlite3TracePatch
+  def execute(sql, bind_vars = [], *args, &block)
+    Datadog::Tracing.trace("sqlite3.execute", service: "isucon-sqlite3", resource: sql) do
+      span = ::Datadog::Tracing.active_span
+      if span
+        vars =
+          if args.empty?
+            bind_vars
+          else
+            Array(bind_vars) + args
+          end
+        span.set_tag("sqlite3.bind_vars", vars)
+      end
+
+      super(sql, bind_vars, *args, &block)
+    end
+  end
+end
+
+SQLite3::Database.prepend(DatadogSqlite3TracePatch)
